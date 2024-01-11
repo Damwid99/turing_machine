3056ba02eec277cb439ac9985100004a722a47f2
Damwid99 <damwid@live.com>
2024-01-11 13:10:04 +0100

    Created functional GUI

--- main.py
+++ main.py
@@ -1,13 +1,144 @@
-from utils.utils import render_txt
-from modules.turingMachine import TuringMachine
+# from utils.utils import render_txt
+# from modules.turingMachine import TuringMachine
 
 
-initial_word, states, final_states, initial_state, relations, tape_alphabet = render_txt('./in.txt')
-machine = TuringMachine(relations=relations, initial_state=initial_state, final_state=final_states, states=states,
-                        initial_word=initial_word)
+# initial_word, states, final_states, initial_state, relations, tape_alphabet = render_txt('./in.txt')
+# machine = TuringMachine(relations=relations, initial_state=initial_state, final_state=final_states, states=states,
+#                         initial_word=initial_word)
 
-print(tape_alphabet )
-
-
-print("-----------------------------------")
 # machine.run()    
+
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtGui import QFont
+from Touring_Gui import Ui_MainWindow 
+from modules.turingMachine import TuringMachine
+from utils.utils import render_txt
+from PyQt5.QtCore import QTimer
+
+
+
+class MyMainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton_2.setEnabled(False)
+        self.ui.pushButton_3.setEnabled(False)
+        fixed_font = QFont("Courier New", 10)
+        self.ui.plainTextEdit_2.setFont(fixed_font)
+        fixed_width = 410  
+        self.ui.plainTextEdit_2.setFixedWidth(fixed_width)
+
+        self.machine = None
+
+        #file uploading
+        self.file_path = ''
+        self.ui.pushButton_4.clicked.connect(self.upload_file)
+
+
+    def upload_file(self):
+        self.file_path = self.ui.lineEdit.text()
+        if not os.path.exists(self.file_path):
+            QMessageBox.warning(self, "File Not Found", f"The file '{self.file_path}' does not exist.")
+            self.file_path = ''
+        else:
+            self.ui.pushButton_2.setEnabled(True)
+            self.ui.pushButton_3.setEnabled(True)
+            QMessageBox.information(self, "File uploaded", f"The file '{self.file_path}' was uploaded succesfully.\nPress play or step forward button to start.")
+            self.start_machine()
+
+
+    def start_machine(self):
+        
+        initial_word, states, final_states, initial_state, relations, tape_alphabet = render_txt(self.file_path)
+        self.machine = TuringMachine(relations=relations, initial_state=initial_state, final_state=final_states, states=states, initial_word=initial_word)
+        
+        text_to_print = self.machine.print_tape(50)
+        self.ui.plainTextEdit_2.appendPlainText(text_to_print)
+        self.ui.pushButton_3.clicked.connect(self.on_button_clicked)
+        self.ui.pushButton_2.clicked.connect(self.step_forward_clicked)
+        self.continuous_printing = False
+
+
+    def on_button_clicked(self):
+        if not self.continuous_printing:
+            self.continuous_printing = True
+            self.update_text_continuously()
+            self.ui.pushButton_2.setEnabled(False)
+        else:
+            self.continuous_printing = False
+            self.ui.pushButton_2.setEnabled(True)
+
+
+    def step_forward_clicked(self):
+        tape_text = self.machine.print_tape(50)
+        relation_to_show = self.machine.update_state()
+
+        self.ui.plainTextEdit_2.clear()
+        self.ui.plainTextEdit_2.appendPlainText(tape_text)
+        self.ui.lineEdit_2.clear()
+        self.ui.lineEdit_2.setText(str(relation_to_show))
+        if self.machine.message:
+            self.ui.lineEdit_3.setText(self.machine.message)
+            QTimer.singleShot(1700, self.clear_message_box)
+        self.check_if_in_final_state(tape_text)
+
+
+
+    def update_text_continuously(self):
+        if self.continuous_printing:
+            text_to_print = self.machine.print_tape(50)
+            relation_to_show = self.machine.update_state()
+            self.ui.plainTextEdit_2.clear()
+            self.ui.plainTextEdit_2.appendPlainText(text_to_print)
+            self.ui.lineEdit_2.clear()
+            self.ui.lineEdit_2.setText(str(relation_to_show))
+            if self.machine.message:
+                self.ui.lineEdit_3.setText(self.machine.message)
+                QTimer.singleShot(1700, self.clear_message_box)
+            # You can adjust the time.sleep value to control the delay between updates.
+            # Here, we're using a short delay of 100 milliseconds.
+            self.check_if_in_final_state(text_to_print)
+            QTimer.singleShot(100, self.update_text_continuously)
+
+
+    def clear_message_box(self):
+        self.ui.lineEdit_3.clear()
+
+
+    def check_if_in_final_state(self, previous_text):
+        if (self.machine.actual_state in self.machine.final_state) or (self.machine.message == "No available states."):
+            self.continuous_printing =False
+            self.ui.pushButton_2.setEnabled(False)
+            self.ui.pushButton_3.setEnabled(False)
+            final_text = '\nInitial word:\n'
+            final_text += ''.join(self.machine.initial_word) + '\n'
+            final_text += 'Final word:\n'
+            final_text += ''.join(self.machine.tape).strip('#') + '\n'
+            final_text += 'Calculating length:\n'
+            final_text += str(self.machine.calculating_length)
+            previous_text += final_text
+            self.ui.plainTextEdit_2.clear()
+            self.ui.plainTextEdit_2.appendPlainText(previous_text)
+            if self.machine.message:
+                self.ui.lineEdit_3.setText(self.machine.message)
+            else:
+                self.ui.lineEdit_3.setText('Ended succesfully.')
+            QTimer.singleShot(5000, self.clear_message_box)
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MyMainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+
+if __name__ == "__main__":
+    main()
--- modules/turingMachine.py
+++ modules/turingMachine.py
@@ -12,6 +12,7 @@ class TuringMachine:
         self.initial_word = initial_word
         self.final_state = final_state
         self.states = states
+        self.calculating_length = 0
 
         self.tape = ['#' for x in range(int((32-len(initial_word))/2))]
         self.tape_index = len(self.tape)
@@ -20,38 +21,48 @@ class TuringMachine:
             self.tape.append('#')
         
         self.actual_state = self.initial_state
+        self.message = ''
 
         self.paused = False
 
     def check_and_extend_tape(self, state):
-        print(f'index: {self.tape_index}    dlugosc: {len(self.tape)}')
         if self.tape_index == 0 and state == 'L':
             for x in range(16):
                 self.tape.insert(0,'#')
             self.tape_index+=16
+            self.message = 'Tape is being extended'
         if self.tape_index == len(self.tape)-1 and state == 'P':
             for x in range(16):
                 self.tape.append('#')
-
+            self.message = 'Tape is being extended'
+        
     def update_state(self):
+        self.message = ''
         realtion_accomplished = False
+        relation_done = None
         for relation in self.relations:
             if relation[0] == self.actual_state and relation[1] == self.tape[self.tape_index]:
-                print(relation)
+                relation_done = relation
                 self.actual_state = relation[2]
                 self.tape[self.tape_index] = relation[3]
                 self.check_and_extend_tape(relation[4])
                 if relation[4] == 'L':
-                    self.tape_index-=1
+                    self.tape_index -= 1
                 else:
-                    self.tape_index+=1
-                realtion_accomplished =True
+                    self.tape_index += 1
+                realtion_accomplished = True
+                self.calculating_length += 1
                 break
         if realtion_accomplished == False:
-            sys.exit()
+            # sys.exit()
+            self.message = "No available states."
+        if relation_done:
+            return ' '.join(relation_done)
+        else:
+            return ""
 
-    def print_tape(self):
-        terminal_width = shutil.get_terminal_size().columns
+    def print_tape(self, terminal_width = shutil.get_terminal_size().columns):
+        # terminal_width = shutil.get_terminal_size().columns
         all_rows = int(len(self.tape)/terminal_width)+1
 
 
@@ -70,17 +81,15 @@ class TuringMachine:
             output += row_output + '\n'  
 
         os.system('cls')
-        print(output)
+        return output
 
     def run(self):
     
         while self.actual_state not in self.final_state:
-
-
             if not self.paused:
-                self.print_tape()
+                print(self.print_tape())
                 print()
-                self.update_state()
+                print(self.update_state())
                 time.sleep(0.1)
 
             if keyboard.is_pressed(' '):
@@ -94,9 +103,9 @@ class TuringMachine:
                     time.sleep(0.1) 
             elif keyboard.is_pressed('right'):
                 print("Stepping forward...")
-                self.print_tape()
+                print(self.print_tape())
                 print()
-                self.update_state()
+                print(self.update_state())
                 time.sleep(0.2)  
             elif keyboard.is_pressed('escape'):
                 sys.exit()
