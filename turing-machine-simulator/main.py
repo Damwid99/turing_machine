efa54180cd5fe2e3a920f64d9d1256f07e9c5bd1
Damwid99 <damwid@live.com>
2024-01-13 17:13:32 +0100

    Fixed printing issue

--- turing-machine-simulator/main.py
+++ turing-machine-simulator/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QFontMetrics
 from GUI.Touring_Gui import Ui_TuringMachine 
 from modules.turingMachine import TuringMachine
 from utils.utils import render_txt
@@ -25,14 +25,18 @@ class MyMainWindow(QMainWindow):
         self.ui.pushButton_3.clicked.connect(self.on_button_clicked)
         self.ui.pushButton_2.clicked.connect(self.step_forward_clicked)
         self.ui.Stop_button.clicked.connect(self.stop_func)
+        self.font_metrics= QFontMetrics(fixed_font)
 
         self.machine = None
+        self.is_final_state = False
+        self.is_ended =False
 
         #file uploading
         self.file_path = ''
         self.ui.pushButton_4.clicked.connect(self.upload_file)  
         # Add the following line to initialize continuous_printing
 
+        self.approx_columns = int(fixed_width/self.font_metrics.width('#'))-5
 
     def upload_file(self):
         file_dialog = QFileDialog(self)
@@ -60,7 +64,7 @@ class MyMainWindow(QMainWindow):
         initial_word, states, final_states, initial_state, relations, tape_alphabet = render_txt(self.file_path)
         self.machine = TuringMachine(relations=relations, initial_state=initial_state, final_state=final_states, states=states, initial_word=initial_word)
         
-        text_to_print = self.machine.print_tape(50)
+        text_to_print = self.machine.print_tape(self.approx_columns)
         self.ui.plainTextEdit_2.appendPlainText(text_to_print)
         
         self.continuous_printing = False
@@ -86,6 +90,8 @@ class MyMainWindow(QMainWindow):
         self.ui.plainTextEdit_2.clear()
         self.ui.lineEdit_2.clear()
         self.ui.lineEdit_3.clear()
+        self.is_ended = False
+        self.is_final_state =False
 
 
     def on_button_clicked(self):
@@ -99,33 +105,43 @@ class MyMainWindow(QMainWindow):
 
 
     def step_forward_clicked(self):
-        tape_text = self.machine.print_tape(50)
-        relation_to_show = self.machine.update_state()
-
-        self.ui.plainTextEdit_2.clear()
-        self.ui.plainTextEdit_2.appendPlainText(tape_text)
-        self.ui.lineEdit_2.clear()
-        self.ui.lineEdit_2.setText(str(relation_to_show))
-        if self.machine.message:
-            self.ui.lineEdit_3.setText(self.machine.message)
-            QTimer.singleShot(1700, self.clear_message_box)
+        tape_text = self.machine.print_tape(self.approx_columns)
         self.check_if_in_final_state(tape_text)
-
-
-    def update_text_continuously(self):
-        if self.continuous_printing:
-            text_to_print = self.machine.print_tape(50)
-            relation_to_show = self.machine.update_state()
+        relation_to_show = self.machine.update_state()
+        if (self.machine.actual_state in self.machine.final_state):
+                self.is_final_state=True
+        if not self.is_ended:
             self.ui.plainTextEdit_2.clear()
-            self.ui.plainTextEdit_2.appendPlainText(text_to_print)
+            self.ui.plainTextEdit_2.appendPlainText(tape_text)
             self.ui.lineEdit_2.clear()
             self.ui.lineEdit_2.setText(str(relation_to_show))
             if self.machine.message:
                 self.ui.lineEdit_3.setText(self.machine.message)
                 QTimer.singleShot(1700, self.clear_message_box)
-            
+        else:
+            self.is_ended = False
+
+
+
+    def update_text_continuously(self):
+        if self.continuous_printing:
+            text_to_print = self.machine.print_tape(self.approx_columns)
             self.check_if_in_final_state(text_to_print)
-            QTimer.singleShot(100, self.update_text_continuously)
+            relation_to_show = self.machine.update_state()
+            if (self.machine.actual_state in self.machine.final_state):
+                self.is_final_state=True
+            if not self.is_ended:
+                self.ui.plainTextEdit_2.clear()
+                self.ui.plainTextEdit_2.appendPlainText(text_to_print)
+                self.ui.lineEdit_2.clear()
+                self.ui.lineEdit_2.setText(str(relation_to_show))
+                if self.machine.message:
+                    self.ui.lineEdit_3.setText(self.machine.message)
+                    QTimer.singleShot(1600, self.clear_message_box)
+                
+                QTimer.singleShot(100, self.update_text_continuously)
+            else:
+                self.is_ended = False
 
 
     def clear_message_box(self):
@@ -133,7 +149,7 @@ class MyMainWindow(QMainWindow):
 
 
     def check_if_in_final_state(self, previous_text):
-        if (self.machine.actual_state in self.machine.final_state) or (self.machine.message == "No available states."):
+        if (self.is_final_state==True) or (self.machine.message == "No available states."):
             self.continuous_printing =False
             self.ui.pushButton_2.setEnabled(False)
             self.ui.pushButton_3.setEnabled(False)
@@ -146,16 +162,19 @@ class MyMainWindow(QMainWindow):
             previous_text += final_text
             self.ui.plainTextEdit_2.clear()
             self.ui.plainTextEdit_2.appendPlainText(previous_text)
-            if self.machine.message:
-                self.ui.lineEdit_3.setText(self.machine.message)
-            else:
+            if self.is_final_state==True:
                 self.ui.lineEdit_3.setText('Ended succesfully.')
+                self.is_ended=True
+            else:
+                self.ui.lineEdit_3.setText(self.machine.message)
             QTimer.singleShot(5000, self.clear_message_box)
+            
+            
             self.ui.Stop_button.setEnabled(False)
             self.ui.pushButton_2.setEnabled(False)
             self.ui.pushButton_3.setEnabled(False)
             self.ui.pushButton_4.setEnabled(True)
-
+        
 
 def main():
     app = QApplication(sys.argv)
